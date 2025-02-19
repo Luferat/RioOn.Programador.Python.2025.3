497e76f6b2459c493c0c6a573f4c9af247ffe2cc
André Luferat <contato@luferat.net>
2025-02-19 11:34:19 -0300

    asdsd

--- aula07/calculadorafl.py
+++ aula07/calculadorafl.py
@@ -5,54 +5,78 @@ from flask import Flask, request
 app = Flask(__name__)
 
 
-@app.route("/")  # Criando a rota da página inicial do site
-def home():
+def fn(numero):
+    """
+    Formata um número para a saída:
+     - Converte para inteiro se ele for inteiro
+     - Troca "." por "," se ele for float
+    ATENÇÃO! Não use nos cálculos...
+    """
+    if numero == int(numero):
+        numero = int(numero)
+    else:
+        numero = str(numero).replace('.', ',')
+    return numero
+
+
+@app.route("/")  # Criando a rota da página inicial (/) do site
+def home():  # Função executada quando a página inicial (/) é solicitada
+
+    # HTML de resposta da página inicial
     return '''
 
-<form action="/calcular" method="post" style="display:table; margin:auto;text-align:center">
-    <h1>Calculadora</h1>
-    <p><input type="number" name="num1" step="0.01" required></p>
-    <p><input type="number" name="num2" step="0.01" required></p>
-    <p>
-        <button type="submit" name="operacao" value="+"> + </button>
-        <button type="submit" name="operacao" value="-"> - </button>
-        <button type="submit" name="operacao" value="*"> × </button>
-        <button type="submit" name="operacao" value="/"> ÷ </button>
-    </p>
-</form>
+        <form action="/calcular" method="post" style="display:table; margin:auto;text-align:center">
+            <h1>Calculadora</h1>
+            <p><input type="number" name="num1" step="0.01" required></p>
+            <p><input type="number" name="num2" step="0.01" required></p>
+            <p>
+                <button type="submit" name="operacao" value="+"> + </button>
+                <button type="submit" name="operacao" value="-"> - </button>
+                <button type="submit" name="operacao" value="*"> × </button>
+                <button type="submit" name="operacao" value="/"> ÷ </button>
+            </p>
+        </form>
 
     '''
 
 
+# A rota '/calcular' só aceita solicitações do tipo "HTTP POST"
 @app.route('/calcular', methods=['POST'])
-def calcular():
+def calcular():  # Função executada quando a rota "/calcular" é solicitada
+
+    # Recebe os dados dos campos do formulário, formata e armazena em variáveis
     num1 = float(request.form['num1'])
     num2 = float(request.form['num2'])
     operacao = request.form['operacao']
 
+    # Apenas para debug
     print('------- DEBUG -----------------', num1, num2, operacao)
 
+    # Realiza a operação solicitada e formata a saída com o resultado
     if operacao == '+':
-        resultado = f'{num1} + {num2} = {num1 + num2}'
+        resultado = f'{fn(num1)} + {fn(num2)} = {fn(num1 + num2)}'
     elif operacao == '-':
-        resultado = f'{num1} - {num2} = {num1 - num2}'
+        resultado = f'{fn(num1)} - {fn(num2)} = {fn(num1 - num2)}'
     elif operacao == '*':
-        resultado = f'{num1} × {num2} = {num1 * num2}'
+        resultado = f'{fn(num1)} × {fn(num2)} = {fn(num1 * num2)}'
     elif operacao == '/':
-        resultado = f'{num1} ÷ {num2} = {num1 / num2}'
+        if num2 != 0:
+            resultado = f'{fn(num1)} ÷ {fn(num2)} = {fn(num1 / num2)}'
+        else:
+            resultado = 'Erro: divisão por zero'
     else:
-        resultado = 'Operação inválida'
+        resultado = 'Erro: Operação inválida'
 
+    # HTML de saída da função
     return f'''
-        <div>
+        <div style="display:table; margin:auto;text-align:center">
             <h1>Calculadora</h1>
-            <h2>{resultado}</h2>
+            <h2><pre>{resultado}</pre></h2>
             <a href="/"> Fazer outro cálculo</a>
         </div>
     '''
 
 
-# Se eu estou em um servidor de desenvolvimento roda o aplicativo
-# Se não, o aplicativo será rodado pelo servidor WSGL (Ex.: Apache → HTTP)
+# Roda o aplicativo se ele for chamado diretamente
 if __name__ == "__main__":
     app.run(debug=True)
